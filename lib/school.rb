42db7b5606cbfcea684f5f5e0b9d1f5c9d38300e
Sharmaine Requerme <srqrme@gmail.com>
2018-08-02 19:53:34 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,15 +8,15 @@ class School
   end
 
   def add_student(grade, student)
-    @roster[grade] ||= []
-    @roster[grade] << student
+    roster[grade] ||= []
+    roster[grade] << student
   end
 
   def grade(grade)
-    @roster[grade]
+    roster[grade]
   end
 
   def sort
-    @roster[grade].sort
+    roster[grade].sort
   end
 end
