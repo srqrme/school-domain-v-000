90816171e16edff94ec9264c634c87167aa2c857
Sharmaine Requerme <srqrme@gmail.com>
2018-08-02 19:45:43 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
 # code here!
+class School
+  attr_accessor :roster
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(grade, student)
+    @roster[grade] = []
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade].sort
+  end
+end
